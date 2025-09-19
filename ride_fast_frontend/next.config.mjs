e49c27b2a1d8dbe0913078858b1ef1424e17574f
ishaa-p7 <ishapatel1207@gmail.com>
2025-09-19 23:12:25 +0530

    Initial commit -login done

---
+++ ride_fast_frontend/next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "s3-ap-southeast-1.amazonaws.com" },
+      { hostname: "olawebcdn.com" },
+      { hostname: "cdn.pixabay.com" },
+    ],
+  },
+  // proxy for api requests
+  async rewrites() {
+  if (process.env.NODE_ENV === "development") {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  } else {
+    return [];
+  }
+}
+,
+};
+
+export default nextConfig;
